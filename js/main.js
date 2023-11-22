5a49663386c097f3e4405532c60f5b031e41142f
Avtar Singh <asm80717@gmail.com>
2023-11-22 13:02:41 +0530

    pop up

--- js/main.js
+++ js/main.js
@@ -88,7 +88,11 @@
             '<i class="bi bi-chevron-right"></i>'
         ]
     });
-
+    $(document).on("click","#cust_btn",function(){
+  
+        $("#myModal").modal("toggle");
+        
+      })
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
